743bb5b2d2fecc32426c24cd27b314431fd715cf
M4C30 <maceosylvanbeltman@gmail.com>
2021-10-11 16:52:47 +0200

    idek anymore

--- home/script.js
+++ home/script.js
@@ -1,7 +1,23 @@
-let mainImage = document.getElementById("imageSlide");
 
-function shadow(id){
- let newID = document.getElementById(id);
- mainImage.style.backgroundImage = "url('../images/homeImages/" + newID.id + ".jpg')";
- newID.style.filter = "opacity(30%)";
+var leftButt = document.getElementById("left");
+var rightButt = document.getElementById("right");
+
+var bgImage = document.getElementById("mainLeft");
+
+leftButt.addEventListener("click", function(){
+    button("left");
+});
+rightButt.addEventListener("click", function(){
+    button("right")
+});
+
+var counter = 0;
+
+function button(slide){
+    var cycle = ["url(../images/homeImages/pic1.jpg)", "url(../images/homeImages/pic2.jpg)", "url(../images/homeImages/pic3.jpg)", "url(../images/homeImages/pic4.jpg)"];
+    if (slide = "right"){
+        bgImage.style.backgroundImage = cycle[0];
+        counter++;
+    }
+
 }
